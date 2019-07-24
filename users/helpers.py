c847cb09fe57d014c8a80c827f20f675a6d50e58
Reico Lee <leereico@gmail.com>
2019-07-23 20:56:38 -0400

    helper class

--- users/helpers.py
+++ users/helpers.py
@@ -1,3 +1,4 @@
+import re
 from django.db.models import Count
 
 from users.models import Users, DisallowedUsernames
@@ -7,30 +8,70 @@ class UsernamesResolutionHelper(object):
     def __init__(self, resolve_type='duplicates', dry_run=True):
         self.type = resolve_type
         self.dry_run = dry_run
-        # self.hash_map = {}
-        self.updated_user_list = []
         self.disallowed_usernames_list = DisallowedUsernames.get_disallowed_usernames_list()
-        self.user_queries = getattr(self, resolve_type)()
+        self.user_queries = getattr(
+            self, '{}_username_queries'.format(resolve_type))()
 
-    def duplicates(self):
-        duplicate_names = Users.objects.values("username").annotate(
+    def duplicates_username_queries(self):
+        duplicate_names_list = Users.objects.values("username").annotate(
             name_count=Count("username")).filter(name_count__gt=1)
 
         user_queries = Users.objects.filter(
-            username__in=[item["username"] for item in duplicate_names]).exclude(username__in=self.disallowed_usernames_list)
+            username__in=[item["username"] for item in duplicate_names_list]).exclude(username__in=self.disallowed_usernames_list)
 
         return user_queries
 
-    def disallowed(self):
+    def disallowed_username_queries(self):
         user_queries = Users.objects.filter(
             username__in=self.disallowed_usernames_list)
         return user_queries
+
+    def update_users(self):
+        updated_user_list = []
+        usernames = {}
+        for user in self.user_queries:
+            if user.username not in usernames:
+                usernames[user.username] = [user.id]
+
+                if self.type == 'disallowed':
+                    list_length = len(usernames[user.username])
+                    user.username = '{}{}'.format(
+                        user.username, str(list_length))
+                    user.save()
+
+            else:
+                usernames[user.username].append(user.id)
+                list_length = len(usernames[user.username])
+
+                if self.type == 'duplicates':
+                    match = re.search('\d', user.username)
+                    username = user.username[:match.start(
+                    )] if match is not None else user.username
+                    num = user.username[match.start(
+                    ):] if match is not None else 0
+                    list_length = int(num) + list_length - 1
+
+                while True:
+                    username = username if self.type == 'duplicates' else user.username
+                    potential_username = '{}{}'.format(
+                        username, str(list_length))
+
+                    if Users.username_exists(potential_username):
+                        list_length += 1
+                    else:
+                        user.username = potential_username
+                        user.save()
+                        break
+
+            updated_user_list.append(user)
+
+        return updated_user_list
 
     def get_users(self):
 
         if self.dry_run:
             return self.user_queries
 
-        # for user in self.user_queries:
+        updated_user_list = self.update_users()
 
-        return
+        return updated_user_list
--- users/views.py
+++ users/views.py
@@ -1,10 +1,6 @@
-import re
-from django.db.models import Count
-
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from users.models import Users, DisallowedUsernames
 from users.serializers import UsersSerializer
 
 from users.helpers import UsernamesResolutionHelper
@@ -19,83 +15,35 @@ def index(request):
 
 @api_view(["GET", "POST"])
 def duplicate_users(request):
-    user_instances = UsernamesResolutionHelper().get_users()
 
     if request.method == "GET":
-        data = UsersSerializer(user_instances, many=True).data
+        affected_users = UsernamesResolutionHelper(
+            resolve_type='duplicates', dry_run=True).get_users()
+        data = UsersSerializer(affected_users, many=True).data
         return Response(data)
 
     if request.method == "POST":
-        hash_map = {}
-        updated_list = []
-        for user in user_instances:
-            if user.username not in hash_map:
-                hash_map[user.username] = [user.id]
-            else:
-                hash_map[user.username].append(user.id)
-                list_length = len(hash_map[user.username])
-
-                match = re.search('\d', user.username)
-                username = user.username[:match.start(
-                )] if match is not None else user.username
-                num = user.username[match.start():] if match is not None else 0
-                digit = int(num) + list_length - 1
-
-                while True:
-                    potential_username = "{}{}".format(
-                        username, str(digit))
-                    if Users.username_exists(potential_username):
-                        digit += 1
-                    else:
-                        user.username = potential_username
-                        user.save()
-                        break
-                # edge case - what if the number is not at the end
-
-        data = UsersSerializer(user_instances, many=True).data
+        updated_user_list = UsernamesResolutionHelper(
+            resolve_type='duplicates', dry_run=False).get_users()
+        data = UsersSerializer(updated_user_list, many=True).data
 
         return Response(data)
 
 
 @api_view(["GET", "POST"])
 def disallowed_users(request):
-    user_instances = user_instances = UsernamesResolutionHelper(
-        resolve_type='disallowed').get_users()
 
     if request.method == "GET":
-        data = UsersSerializer(user_instances, many=True).data
+        affected_users = UsernamesResolutionHelper(
+            resolve_type='disallowed', dry_run=True).get_users()
+        data = UsersSerializer(affected_users, many=True).data
         return Response(data)
 
     elif request.method == "POST":
-        updated_list = []
-        hash_map = {}
-        for user in user_instances:
-            if user.username not in hash_map:
-                hash_map[user.username] = [user.id]
-                list_length = len(hash_map[user.username])
-
-                user.username = "{}{}".format(user.username, str(list_length))
-                user.save()
-            else:
-                hash_map[user.username].append(user.id)
-                list_length = len(hash_map[user.username])
-
-                while True:
-                    potential_username = "{}{}".format(
-                        user.username, str(list_length))
-
-                    if Users.username_exists(potential_username):
-                        list_length += 1
-                    else:
-                        user.username = potential_username
-                        user.save()
-                        break
-
-                # edge case - need to check if it collides with existing user names with potential usernames
-                # edge case - what if there are numeric value at the end such as about1
-
-            updated_list.append(user)
-
-        data = UsersSerializer(updated_list, many=True).data
+        updated_user_list = UsernamesResolutionHelper(
+            resolve_type='disallowed', dry_run=False).get_users()
+        data = UsersSerializer(updated_user_list, many=True).data
 
         return Response(data)
+        # edge case - need to check if it collides with existing user names with potential usernames
+        # edge case - what if there are numeric value at the end such as about1
